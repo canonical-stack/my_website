361e2a3ab6b4c9e5e3d14c7bec2890f1c3a7beaf
canonical-stack <amig01989amigos@gmail.com>
2025-12-26 17:55:16 +0300

    изменения 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact'; // ← Обратите внимание: Contact, а не Contacts
 
 import './App.css';
@@ -19,7 +18,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} /> {/* ← Используем /contact, а не /contacts */}
       </Routes>
       <Footer />
